90e06eade0bf346681d7f175c7189c0394c54a2f
poojapatil <156627026+patil-poooja@users.noreply.github.com>
2025-01-19 14:13:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let x = document.getElementById("fname");
+x.addEventListener("onfocusout", myFocusFunction, true);
+
+function myFocusFunction() {
+  //alert("called");
+  let text=document.getElementById("fname").value;
+  document.getElementById("fname").value=text.toUpperCase();
+}
